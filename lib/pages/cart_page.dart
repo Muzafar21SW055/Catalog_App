a9c1a69e970396f4951d730dfcb4ee4cbc2bb493
Muzafar21SW055 <21sw055@students.muet.edu.pk>
2024-11-14 21:22:27 +0500

    day21 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:learningdart/pages/home_detail_page.dart';
 import 'package:learningdart/widgets/themes.dart';
@@ -13,6 +14,62 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: context.canvasColor,
         title: "Cart".text.color(context.colors.secondary).bold.xl3.make(),
+      ),
+      body: Column(
+        children: [
+          _CartList().p16().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "Total: \$9999".text.xl4.color(context.colors.secondary).make().p20(),
+          ElevatedButton(
+            style: ButtonStyle(
+              foregroundColor:
+                  MaterialStateProperty.all(context.colors.primary),
+              backgroundColor:
+                  MaterialStateProperty.all(context.colors.secondary),
+            ),
+            onPressed: () {},
+            child: "Buy".text.color(context.colors.primary).xl2.make(),
+          ).w32(context),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({super.key});
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(CupertinoIcons.check_mark_circled_solid),
+        trailing: IconButton(
+            onPressed: () {}, icon: Icon(Icons.remove_circle_outline)),
+        title: "Item 1".text.textStyle(context.captionStyle).make(),
       ),
     );
   }
